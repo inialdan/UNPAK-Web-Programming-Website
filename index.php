d293a7b5743abacbecf3ee0c05e2a47ff06a5c44
Aldan Rizki Santosa <projectarizki@gmail.com>
2021-03-25 20:05:37 +0700

    Upload projects

---
+++ index.php
@@ -0,0 +1,227 @@
+<!DOCTYPE HTML>
+<!--
+	Transitive by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Web Programming</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header" class="alt">
+				<div class="logo"><a href="index.html">Web Programming <span>Kelompok 1</span></a></div>
+				<a href="#menu" class="toggle"><span>Menu</span></a>
+			</header>
+
+		<!-- Nav -->
+			<nav id="menu">
+				<ul class="links">
+					<li><a href="index.html">Home</a></li>
+					<li><a href="generic.html">Generic</a></li>
+					<li><a href="elements.html">Elements</a></li>
+				</ul>
+			</nav>
+
+		<!-- Banner -->
+		<!--
+			To use a video as your background, set data-video to the name of your video without
+			its extension (eg. images/banner). Your video must be available in both .mp4 and .webm
+			formats to work correctly.
+		-->
+			<section id="banner" data-video="images/banner">
+				<div class="inner">
+					<h1>Web Programming</h1>
+					<p>Universitas Pakuan - Kelompok 1<br />
+					<a href="#one" class="button special scrolly">Mulai</a>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<div>
+						<div class="box">
+							<div class="image fit">
+								<img src="images/pic01.jpg" alt="" />
+							</div>
+							<div class="content">
+								<header class="align-center">
+									<h2>Pengenalan</h2>
+									<p>Web Programming</p>
+								</header>
+								<hr />
+								<p> 
+									Web programming atau Pengembangan Web adalah istialh yang luas untuk pekerjaan yang terlibat dalam mengembangkan suatu situs web untuk internet atau intranet. Web Programming terdiri dari dua suku kata yaitu web dan programming. Programming dapat diartikan sebagai proses, cara, atau pembuatan suatu program. Sedangkan web dapat diartikan sebagai jaringan komputer yang terdiri dari kumpulan situs internet yang menawarkan teks, grafik, suara dan sumber daya animasi melalui protokol transfer hypertext (HTTP). Jadi, web programming adalah suatu proses, cara, atau pembuatan suatu program berbasis web. Dalam memahami web programming, mungkin terbesit di dalam benak Anda harus darimana Anda memulai dalam mempelajari web programming.
+								</p>
+								<p>
+									<center><b>Tiga Komponen Utama Web Programming</b></center>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style3">
+				<div class="inner">
+					<div id="flexgrid">
+						<div>
+							<header>
+								<h3>HTML</h3>
+							</header>
+							<p>Hypertext Markup Language adalah bahasa markah standar untuk dokumen yang dirancang untuk ditampilkan di peramban internet. Ini dapat dibantu oleh teknologi seperti Cascading Style Sheets dan bahasa scripting seperti JavaScript dan VBScript.</p>
+							<ul class="actions">
+								<!-- <li><a href="#" class="button alt">Learn More</a></li> -->
+							</ul>
+						</div>
+						<div>
+							<header>
+								<h3>CSS</h3>
+							</header>
+							<p>Cascading Style Sheet merupakan aturan untuk mengatur beberapa komponen dalam sebuah web sehingga akan lebih terstruktur dan seragam. CSS bukan merupakan bahasa pemograman.</p>
+							<ul class="actions">
+								<!-- <li><a href="#" class="button alt">Learn More</a></li> -->
+							</ul>
+						</div>
+						<div>
+							<header>
+								<h3>JavaScript</h3>
+							</header>
+							<p>JavaScript adalah bahasa pemrograman tingkat tinggi dan dinamis. JavaScript populer di internet dan dapat bekerja di sebagian besar penjelajah web populer seperti Google Chrome, Internet Explorer, Mozilla Firefox, Netscape dan Opera. Kode JavaScript dapat disisipkan dalam halaman web menggunakan tag SCRIPT.</p>
+							<ul class="actions">
+								<!-- <li><a href="#" class="button alt">Learn More</a></li> -->
+							</ul>
+						</div>
+					</div>
+				</div>
+			</section>
+
+		<!-- Three -->
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div>
+							<div class="box">
+								<div class="image fit">
+									<img src="images/pic02.jpg" alt="" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<h2>Sejarah Internet</h2>
+									</header>
+									<hr />
+									<p> 
+										Berbicara tentang pemrograman web tentu tidak bisa lepas dari internet. Karena web merupakan bagian dari internet, maka tidak ada salahnya kita sedikit menapak tilas tentang sejarah internet.
+									</p>
+									<p>
+										Awalnya, internet merupakan sebuah proyek riset tingkat tinggi yang dimiliki oleh Departemen Pertahanan Amerika Serikat yang bernama ARPANET. Saat pertama kali lahir, ARPANET hanya menghubungkan 4 situs saja yaitu Stanford Research Institute, University of California, Santa Barbara dan University of Utah.
+									</p>
+									<p>
+										Pada Tahun 1984 diperkenalkan sistem nama domain yang kita kenal sekarang dengan nama DNS (Domain Name System). Dengan sistem ini memungkinkan untuk menyeragamkan sistem pemberian nama alamat di jaringan komputer yang sebelumnya hanya berupa susunan angka yang disebut IP Address.
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="image fit">
+									<img src="images/pic03.jpg" alt="" />
+								</div>
+								<div class="content">
+									<header class="align-center">
+										<h2>Lini Masa Perkembangan Pemrograman Web</h2>
+									</header>
+									<hr />
+									<p>
+										Pada bulan Oktober 1990, Tim Berners-Lee menspesifikasikan tiga teknologi fundamental yang menjadi cikal bakal web yang kita sering akses hari ini. Tiga teknologi tersebut adalah :
+									</p>
+									<p>
+										1. HTML: HyperText Markup Language. Format umum yang digunakan dalam pemrograman web, termasuk kemampuan untuk menghubungkan satu dokumen ke dokumen lainnya
+									</p>
+									<p>
+										2. URI: Uniform Resource Identifier. Merupakan sebuah alamat unik yang menjadi ciri dari sebuah dokumen pada suatu web.
+									</p>
+									<p>
+										3. HTTP: HyperText Transfer Protocol. Sebuah protocol yang digunakan untuk menghubungkan setiap dokumen yang tersebar di internet.
+									</p>
+									<p>
+										Akhirnya pada 1994 lahirlah World Wide Web Consortium (W3C). Sebagai sebuah tempat untuk menyeragamkan spesifikasi serta petunjuk-petunjuk pemrograman web supaya dapat bekerja untuk semua orang sesuai dengan kepentingan masing-masing.
+									</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+		<!-- Four -->
+			<section id="four" class="wrapper style3">
+				<div class="inner">
+
+					<header class="align-center">
+						<h2>Quote</h2>
+						<p>
+							“A website without visitors is like a ship lost in the horizon.”
+							<br/>
+							– Dr. Christopher Dayagdag
+						</p>
+					</header>
+
+				</div>
+			</section>
+
+		<!-- Footer -->
+		
+			<footer id="footer" class="wrapper">
+				<div class="inner">
+					<section>
+						<div class="box">
+							<div class="content">
+								<h2 class="align-center">Hubungi Kami</h2>
+								<hr />
+								<form action="#" method="post">
+									<div class="field half first">
+										<label for="name">Nama</label>
+										<input name="name" id="name" type="text" placeholder="Name">
+									</div>
+									<div class="field half">
+										<label for="email">Email</label>
+										<input name="email" id="email" type="email" placeholder="Email">
+									</div>
+									<div class="field">
+										<label for="message">Pesan</label>
+										<textarea name="message" id="message" rows="6" placeholder="Message"></textarea>
+									</div>
+									<ul class="actions align-center">
+										<li><input value="Kirim Pesan" class="button special" type="submit"></li>
+									</ul>
+								</form>
+							</div>
+						</div>
+					</section>
+					<div class="copyright">
+						&copy; Universitas Pakuan - Web Programming
+					</div>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
