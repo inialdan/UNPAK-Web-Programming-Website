4804a26a9431a4606451cabdb7ffd6d9b3be9016
Aldan Rizki Santosa <projectarizki@gmail.com>
2021-03-25 20:23:55 +0700

    Create page login, logout, post, profile and register

---
+++ login.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<!--
+	Transitive by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Web Programming</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+			<section id="three" class="wrapper style2">
+				<div class="inner">
+					<section>
+						<div class="box">
+							<div class="content">
+								<h2 class="align-center">Masuk</h2>
+								<hr />
+								<form action="#" method="post">
+									<center>
+										<div class="field half">
+											<input name="username" id="username" type="text" placeholder="Username">
+										</div>
+										<br>
+										<div class="field half">
+											<input name="password" id="password" type="password" placeholder="Password">
+										</div>
+										<ul class="actions align-center">
+											<li><input value="Login" class="button special" type="submit"></li>
+										</ul>
+										<a href="register.php">Belum punya akun?</a>
+									</center>
+								</form>
+							</div>
+						</div>
+					</section>
+					<div class="copyright">
+						&copy; Universitas Pakuan - Web Programming
+					</div>
+				</div>
+			</section>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
---
+++ post.php
@@ -0,0 +1,102 @@
+<!DOCTYPE HTML>
+<!--
+	Transitive by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>Web Programming</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+	<body>
+			<section id="one" class="wrapper style2">
+				<div class="inner">
+					<section>
+						<div class="box">
+							<div class="content">
+								<h2 class="align-center">Hi, {$Nama}</h2>
+								<hr />
+								<form action="#" method="post">
+									<center>
+										Ceritakan Harimu <br/>
+										<div class="field">
+											<label for="message">Pesan</label>
+											<textarea name="message" id="message" rows="6" placeholder="Ceritakan harimu disini ya"></textarea>
+										</div>
+										<ul class="actions align-center">
+											<li><input value="Bagikan" class="button special" type="submit"></li>
+										</ul>
+									</center>
+								</form>
+							</div>
+						</div>
+					</section>
+				</div>
+			</section>
+
+			<section id="two" class="wrapper style2">
+				<div class="inner">
+					<div class="grid-style">
+
+						<div>
+							<div class="box">
+								<div class="content">
+									<p> 
+										Berbicara tentang pemrograman web tentu tidak bisa lepas dari internet. Karena web merupakan bagian dari internet, maka tidak ada salahnya kita sedikit menapak tilas tentang sejarah internet.
+									</p>
+									<p>
+										Awalnya, internet merupakan sebuah proyek riset tingkat tinggi yang dimiliki oleh Departemen Pertahanan Amerika Serikat yang bernama ARPANET. Saat pertama kali lahir, ARPANET hanya menghubungkan 4 situs saja yaitu Stanford Research Institute, University of California, Santa Barbara dan University of Utah.
+									</p>
+									<p>
+										Pada Tahun 1984 diperkenalkan sistem nama domain yang kita kenal sekarang dengan nama DNS (Domain Name System). Dengan sistem ini memungkinkan untuk menyeragamkan sistem pemberian nama alamat di jaringan komputer yang sebelumnya hanya berupa susunan angka yang disebut IP Address.
+									</p>
+								</div>
+							</div>
+						</div>
+
+						<div>
+							<div class="box">
+								<div class="content">
+									<p>
+										Pada bulan Oktober 1990, Tim Berners-Lee menspesifikasikan tiga teknologi fundamental yang menjadi cikal bakal web yang kita sering akses hari ini. Tiga teknologi tersebut adalah :
+									</p>
+									<p>
+										1. HTML: HyperText Markup Language. Format umum yang digunakan dalam pemrograman web, termasuk kemampuan untuk menghubungkan satu dokumen ke dokumen lainnya
+									</p>
+									<p>
+										2. URI: Uniform Resource Identifier. Merupakan sebuah alamat unik yang menjadi ciri dari sebuah dokumen pada suatu web.
+									</p>
+									<p>
+										3. HTTP: HyperText Transfer Protocol. Sebuah protocol yang digunakan untuk menghubungkan setiap dokumen yang tersebar di internet.
+									</p>
+									<p>
+										Akhirnya pada 1994 lahirlah World Wide Web Consortium (W3C). Sebagai sebuah tempat untuk menyeragamkan spesifikasi serta petunjuk-petunjuk pemrograman web supaya dapat bekerja untuk semua orang sesuai dengan kepentingan masing-masing.
+									</p>
+								</div>
+							</div>
+						</div>
+
+					</div>
+				</div>
+			</section>
+
+			<footer id="footer" class="wrapper">
+				<div class="inner">
+					<div class="copyright">
+						&copy; Universitas Pakuan - Web Programming
+					</div>
+				</div
+			</footer>
+
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
